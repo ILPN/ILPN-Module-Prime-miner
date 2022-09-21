c0d2368414485e2feeaf55e0710de6b2832443ff
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-09-21 15:31:32 +0200

    removed most configuration options

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {
-    AbelOracleService,
     AlgorithmResult,
     AlphaOracleService,
     DropFile,
@@ -28,13 +27,10 @@ export class AppComponent {
     public fdPn = FD_PETRI_NET;
 
     public fcOracle: FormControl;
-    public fcAlphaLookAheadDistance: FormControl;
     public fcAlphaCleanLog: FormControl;
     public fcAlphaDistinguishSameEvents: FormControl;
     public fcAlphaStartStop: FormControl;
-    public fcAlphaRemovePrefixes: FormControl;
 
-    public fcOneBound: FormControl;
     public fcEmptyRegions: FormControl;
 
     public log: Array<Trace> | undefined;
@@ -42,19 +38,15 @@ export class AppComponent {
     public processing = false;
 
     constructor(private _logParser: XesLogParserService,
-                private _αbelOracle: AbelOracleService,
                 private _alphaOracle: AlphaOracleService,
                 private _logTransformer: LogToPartialOrderTransformerService,
                 private _primeMiner: PrimeMinerService,
                 private _netSerializer: PetriNetSerialisationService) {
-        this.fcOracle = new FormControl('alpha');
-        this.fcAlphaLookAheadDistance = new FormControl('1');
+        this.fcOracle = new FormControl('none');
         this.fcAlphaCleanLog = new FormControl(true);
         this.fcAlphaDistinguishSameEvents = new FormControl(false);
         this.fcAlphaStartStop = new FormControl(false);
-        this.fcAlphaRemovePrefixes = new FormControl(true);
 
-        this.fcOneBound = new FormControl(false);
         this.fcEmptyRegions = new FormControl(false);
     }
 
@@ -80,7 +72,7 @@ export class AppComponent {
 
             console.debug(pos);
             this._primeMiner.mine(pos, {
-                oneBoundRegions: this.fcOneBound.value,
+                oneBoundRegions: true,
                 noOutputPlaces: this.fcEmptyRegions.value
             }).subscribe({
                 next: result => {
@@ -99,20 +91,21 @@ export class AppComponent {
     }
 
     private convertLogToPOs(): Observable<Array<PetriNet>> {
-        if (this.fcOracle.value === 'alpha') {
+        if (this.fcOracle.value === 'alpha' || this.fcOracle.value === 'none') {
             const concurrency = this._alphaOracle.determineConcurrency(this.log!, {
                 distinguishSameLabels: this.fcAlphaDistinguishSameEvents.value,
-                lookAheadDistance: this.fcAlphaLookAheadDistance.value === '*' ? Number.POSITIVE_INFINITY : Number.parseInt(this.fcAlphaLookAheadDistance.value),
+                lookAheadDistance: this.fcOracle.value === 'none' ? 0 : 1,
             });
             const pos = this._logTransformer.transformToPartialOrders(this.log!, concurrency, {
                 cleanLog: this.fcAlphaCleanLog.value,
-                discardPrefixes: this.fcAlphaRemovePrefixes.value,
+                discardPrefixes: true,
                 addStartStopEvent: this.fcAlphaStartStop.value
             });
             return of(pos);
         } else {
-            // αbel
-            return this._αbelOracle.determineConcurrency(this.log!);
+            // timestamp
+            // TODO
+            return of([]);
         }
     }
 
